07fba93d866f2e1c07110c048207e1aa25429dd1
jeremias-jalil <jjejalil@gmail.com>
2021-08-19 09:41:51 -0300

    new port

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { conn } = require('./src/db.js');
 const axios = require('axios');
 const { Genre, Platform } = require('./src/db');
 const e = require('express');
-const { API_KEY, API } = process.env;
+const { API_KEY, API,PORT } = process.env;
 
 
 // Syncing all the models at once.
@@ -70,7 +70,7 @@ conn.sync({ force: false }).then(async () => {
 
   }
 
-  server.listen(3001, () => {
+  server.listen(PORT || 3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
